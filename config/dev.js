72ef8849a6b3d7a8d758f50aaf9de342d348d022
ShaoZH923 <z5300917@ad.unsw.edu.au>
2022-06-15 16:53:42 +0800

    Config the koa server so it could run

---
+++ config/dev.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: 'dev',
+    port: 700,
+    // mysql: {
+    //     aws: {
+    //         host: 'cs407jialudb.c0ughbdye66r.us-east-2.rds.amazonaws.com',
+    //         username: 'cs407jialu',
+    //         database: 'cs407dba',
+    //         password: 'cs407pass',
+    //         waitForConnections: true,
+    //         connectionLimit: 10,
+    //         queueLimit: 0
+    //     }
+    // },
+    // s3: {
+    //     signatureVersion: 'v4',
+    //     apiVersion: '2006-03-01',
+    //     region: 'us-east-2'
+    // },
+    // keys: {
+    //     jwt: "cs407summer19modworkshop"
+    // }
+}
